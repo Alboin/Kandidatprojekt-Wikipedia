48ffcda26fc2036263a7ae3594491f4cedb3b8a6
hanna johansson <hanna93.hej@gmail.com>
2016-03-18 13:52:22 +0100

    added comments

--- js/app.js
+++ js/app.js
@@ -1,17 +1,20 @@
+// Hanna och Johanna har skrivit den här filen från början. Den är skriven med angular.js.
+
 
 (function(){
-		
-	var article = { 
-		title: 'Sverige', 
-		coord: 63.16,
-		found: true }; 
+	
+	//article är ett objekt som skulle kunna ha olika attribut, så som 'title' och 'coord'.	
+	//Just nu har den inga attribut, men eventuellt skulle vi kunna ha det framöver?
+	var article = {}; 
 
 	var app = angular.module('wikiSearch', []);
 
+	//Den här controllern kontrollerar artiklarna.
 	app.controller('ArticleController', function(){
 		this.product = article;
 	});
 
+	//Den här controllern kontrollerar "sidorna" i webbapplikationen
 	app.controller('PageController', function(){
 		this.page = 4;
 
