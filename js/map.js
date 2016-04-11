a67228dd22cb8d2317d8a49bc2d72dfae74e56d7
Albin Bergström <albinbergstrom@gmail.com>
2016-04-11 16:24:11 +0200

    Lyckats lägga till ett svg-element ovanpå kartan där man kan rita ut saker.

--- js/map.js
+++ js/map.js
@@ -3,4 +3,9 @@ function generateMap(coordinate) {
 
 	var map = L.mapbox.map('map', 'mapbox.light')
     .setView([coordinate[0], coordinate[1]], 9);
+
+    var svg = d3.select(map.getPanes().overlayPane).append("svg");
+    var g = svg.append("g").attr("class", "leaflet-zoom-hide");
+
+    svg.append("circle").attr("cx", 25).attr("cy", 25).attr("r", 20).attr("id", "svart").style("fill", "green");
 }
