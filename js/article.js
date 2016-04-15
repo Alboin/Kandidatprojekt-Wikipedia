0a04ea80b0e98cc8f7156cdc393e17a09355d84d
hanna johansson <hanna93.hej@gmail.com>
2016-04-15 11:39:57 +0200

    changed comments to English

--- js/article.js
+++ js/article.js
@@ -1,9 +1,18 @@
 
 /*******************************************************************************************************
- 	Hämtar information från Wikipedia och deklarerar olika variabler för varje data som hämtats från APIt.
- 	Datan kan användas i index och de olika js-filerna. 
- 	Det var Albin som ursprungligen skrev denna kod.
+ 	Authors: Originally Albin Bergström who has written this code.
 
+ 	Gets information from Wikipedia and declares different variables for every data that is handled by 
+ 	the Wikipedia API. The data can be used in the index-file and the different js-files.
+
+ 	The file includes the functions:
+ 	- runProgram
+ 	- getSearchString
+ 	- searchWiki
+ 	- load
+ 	- getPosition
+ 	- printArticle
+ 	- getFirstRows
 ********************************************************************************************************/
 		
 
@@ -17,227 +26,226 @@ var save;
 var first_time;
 
 
-		//Funktionen som körs när man trycker på "search"
-		function runProgram() {		
+//The function is run when the user press "search"
+function runProgram() {		
 
-			var usertext = document.getElementById("searchtext").value;
+	var usertext = document.getElementById("searchtext").value;
 
-			var query = getSearchString(usertext);
-			//Denna funktion körs asynkront.
-			first_time = true;
-			searchWiki(query, first_time);
-			//Det betyder att HÄR, efter funktionen skulle man kunna ha någon slags loadingscreen, som visas medans vi väntar på ett svar från funktionen.
-		}
+	var query = getSearchString(usertext);
+	//This function is run asynchronously.
+	first_time = true;
+	searchWiki(query, first_time);
+	//HERE you could have some type of loadingscreen that is shown while waiting for a response from the function.
+	
+}
 
 		
-		//Funktionen kollar att söksträngen inte är tom, byter ut mellanslag
-		//mot "%20" (för att wikipedia vill det) och lägger sedan in den i en hårdkodad query.
-		//Till sist skickas den färdiga query:n till funktionen "searchWiki".
-		function getSearchString(input_title) {
-			if(input_title) {
-				input_title = input_title.replace(" ", "%20");
+/*	The function check if the searchstring is empty and replace spaces with "%20",
+	because this is how Wikipedia's API works. The function then adds the searchstring in a query.
+	At last the final query is sent to the function 'searchWiki'. */
+function getSearchString(input_title) {
+	if(input_title) {
+		input_title = input_title.replace(" ", "%20");
 
-				//The beginning of the query, tells us to do a query and return the result on json format.
-				var start = "/w/api.php?action=query&format=json";
-				//The what to search for
-				var title = "&titles=" + input_title;
+		//The beginning of the query, tells us to do a query and return the result on json format.
+		var start = "/w/api.php?action=query&format=json";
 
-				//Which properties to get. (coordinates, links, revisions, extracts, pageid, pageimages, images(används inte än), categories)
-				var properties = "&prop=coordinates%7Clinks%7Crevisions%7Cextracts%7Cpageimages%7Cimages%7Ccategories" + "&indexpageids=1" + "&pllimit=max"; 
-				var revisions = "&rvprop=content" + "&exintro=1" + "&explaintext=1";
+		//The what to search for
+		var title = "&titles=" + input_title;
 
-				//Which lists to get.
-				var lists = "&list=backlinks"
-				var list_parameters = "&bllimit=max" + "&bltitle=" + input_title;
-				//Create final query
-				var finalQuery = "http://sv.wikipedia.org" + start + title + properties + revisions + lists + list_parameters + "&callback=?";
-			    return finalQuery;
-			}
-		}
+		//Which properties to get. (coordinates, links, revisions, extracts, pageid, pageimages, images(används inte än), categories)
+		var properties = "&prop=coordinates%7Clinks%7Crevisions%7Cextracts%7Cpageimages%7Cimages%7Ccategories" + "&indexpageids=1" + "&pllimit=max"; 
+		var revisions = "&rvprop=content" + "&exintro=1" + "&explaintext=1";
 
-		//Funktionen tar en färdig query som input, kör en GET vad nu det innebär, får ett json-objekt som svar, 
-		//lagrad i variabeln "data".
-		function searchWiki(query, first){
-			$(document).ready(function(){
-			    $.ajax({
-			        type: "GET",
-			        url: query,
-			        contentType: "application/json; charset=utf-8",
-			        async: true,
-			        dataType: "json",
-			        success: function (data, textStatus, jqXHR) {
+		//Which lists to get.
+		var lists = "&list=backlinks"
+		var list_parameters = "&bllimit=max" + "&bltitle=" + input_title;
 
-			        	if(first){
+		//Create final query
+		var finalQuery = "http://sv.wikipedia.org" + start + title + properties + revisions + lists + list_parameters + "&callback=?";
+	    return finalQuery;
+	}
+}
 
-			        		handleLinks(load(data).links);		//gör en sökning för länkarna 'links'
-			        		handleLinks(load(data).backlinks);	//gör en sökning för länkarna 'backlinks'
-			        		printArticle(load(data));			//gör en sökning för den sökta artikeln
+//The function takes a final query as input, uses GET (sends a query to the Wikipedia API) 
+//A json-object is returned, saved in the variable 'data'.
+function searchWiki(query, first){
+	$(document).ready(function(){
+	    $.ajax({
+	        type: "GET",
+	        url: query,
+	        contentType: "application/json; charset=utf-8",
+	        async: true,
+	        dataType: "json",
+	        success: function (data, textStatus, jqXHR) {
 
-			        		console.log(all_articles[0]); 	//Log all articles in console
-			       
-			        		//Get first sentence in a paragraph. 
-			        		getFirstRow(all_articles[0].first_paragraph);
+	        	if(first){
 
-			        	}
-			        	else{
+	        		handleLinks(load(data).links);		//does a search for 'links'
+	        		handleLinks(load(data).backlinks);	//does a search for 'backlinks'
+	        		printArticle(load(data));			//does a search for the chosen article
 
-			        		//Hantera de relaterade länkarna, gör en ny sökning för varje relaterad länk.
-			        		printLinks(loadLinks(data));
-			        	}
-			        },
+	        		console.log(all_articles[0]); 		//Log all articles in console
+	       
+	        		//Get first sentence in a paragraph. 
+	        		getFirstRow(all_articles[0].first_paragraph);
 
-			        //Error message in console, om ingen sökning gjorts.
-			        error: function (errorMessage) {
-			        	console.log("Inget sökord ifyllt.");
-			        }
-			    });
-			});
-		}
+	        	}
+	        	else{
 
-		//Hämtar datainformation om en artikel och sparar i 'temp_article'
-		function load(data) {
-			var temp_article = {
-				title: "",
-				id: -1,
-				links: [],
-				backlinks: [],
-				first_paragraph: "",
-				position: [null,null],
-				birthplace: "",
-				time: [null, null],
-				image_source: "",
-				categories: "", 
-				first_sentence:""
+	        		//Handle the related articles, make a new search for each one of them.
+	        		printLinks(loadLinks(data));
+	        	}
+	        },
 
-			}
+	        //Error message in console, if no search is made by the user.
+	        error: function (errorMessage) {
+	        	console.log("Inget sökord ifyllt.");
+	        }
+	    });
+	});
+}
 
-			//Loop through array of backlinks
-			for(var indx = 0; indx < data.query.backlinks.length; indx++) {
-				temp_article.backlinks.push(data.query.backlinks[indx].title);
-			}
-			
-			temp_article.id = data.query.pageids[0];						//Save article id
-			temp_article.title = data.query.pages[temp_article.id].title;	//Save the title of the article
-			temp_article.first_paragraph = data.query.pages[temp_article.id].extract;			//Save first paragraph of the article
-			temp_article.image_source = data.query.pages[temp_article.id].thumbnail.source;		//Save small image, source
-			//temp_article.image_large = data.query.pages[temp_article.id].thumbnail.source;
-			//temp_article.categories = data.query.pages[temp_article.id].categories;//.title;
+//Gets data for an article and saves it in 'temp_article'
+function load(data) {
+	var temp_article = {
+		title: "",
+		id: -1,
+		links: [],
+		backlinks: [],
+		first_paragraph: "",
+		position: [null,null],
+		birthplace: "",
+		time: [null, null],
+		image_source: "",
+		categories: "", 
+		first_sentence:""
 
-			//Loop through array of links
-			for(var indx = 0; indx < data.query.pages[temp_article.id].links.length; indx++) {
-				//Save titles of the links
-				temp_article.links.push(data.query.pages[temp_article.id].links[indx].title);
-			}
+	}
 
-			//Check if the article has coordinates
-			if(data.query.pages[temp_article.id].coordinates) {
-				//Save coordinates in 'temp_article.position'
-				temp_article.position =
-					[data.query.pages[temp_article.id].coordinates[0].lat,	//latitud
-					 data.query.pages[temp_article.id].coordinates[0].lon]	//longitud
-			} else {
-				temp_article.position = [null,null];
-			}
-			
-			//Get time mentioned in first paragraph of the article
-			temp_article.time = getTime(temp_article.first_paragraph);
-			//Get position of birthplace
-			temp_article.birthplace = getPosition(data.query.pages[temp_article.id].revisions[0]["*"]);
-			//To get the first row in a paragraph. 
-			temp_article.first_sentence=getFirstRow(temp_article.first_paragraph);
+	//Loop through an array of backlinks
+	for(var indx = 0; indx < data.query.backlinks.length; indx++) {
+		temp_article.backlinks.push(data.query.backlinks[indx].title);
+	}
+	
+	temp_article.id = data.query.pageids[0];											//Save article id
+	temp_article.title = data.query.pages[temp_article.id].title;						//Save the title of the article
+	temp_article.first_paragraph = data.query.pages[temp_article.id].extract;			//Save first paragraph of the article
+	temp_article.image_source = data.query.pages[temp_article.id].thumbnail.source;		//Save small image, source
+	//temp_article.image_large = data.query.pages[temp_article.id].thumbnail.source;
+	//temp_article.categories = data.query.pages[temp_article.id].categories;//.title;
 
-			all_articles.push(temp_article);
+	//Loop through an array of links
+	for(var indx = 0; indx < data.query.pages[temp_article.id].links.length; indx++) {
+		//Save titles of the links
+		temp_article.links.push(data.query.pages[temp_article.id].links[indx].title);
+	}
 
-			return temp_article;
-		}
+	//Check if the article has coordinates
+	if(data.query.pages[temp_article.id].coordinates) {
+		//Save coordinates in 'temp_article.position'
+		temp_article.position =
+			[data.query.pages[temp_article.id].coordinates[0].lat,	//latitude
+			 data.query.pages[temp_article.id].coordinates[0].lon]	//longitude
+	} else {
+		temp_article.position = [null,null];
+	}
+	
+	//Get time mentioned in first paragraph of the article
+	temp_article.time = getTime(temp_article.first_paragraph);
+	//Get position of birthplace
+	temp_article.birthplace = getPosition(data.query.pages[temp_article.id].revisions[0]["*"]);
+	//To get the first row in a paragraph. 
+	temp_article.first_sentence=getFirstRow(temp_article.first_paragraph);
 
+	all_articles.push(temp_article);
 
-		//Get position of birthplace
-		function getPosition(revision) {
-			
-			var birthplace = "";
-			var indx = revision.indexOf("f\u00f6delseplats");
-
-			indx = revision.indexOf("[[", indx) + 2;
-			birthplace = revision.substring(indx, revision.indexOf("]]",indx));
-
-			return birthplace;
-		}
+	return temp_article;
+}
 
 
+//Get position of birthplace
+function getPosition(revision) {
+	
+	var birthplace = "";
+	var indx = revision.indexOf("f\u00f6delseplats");
 
-/*-----------------------------------------------
-			Printa ut massa info! :) 
------------------------------------------------*/
-		function printArticle(article) {
+	indx = revision.indexOf("[[", indx) + 2;
+	birthplace = revision.substring(indx, revision.indexOf("]]",indx));
 
-			/*-----------------------------------------------
-					For the modal popup 
-			-----------------------------------------------*/
-			//Title
-			document.getElementById("artikel_titel").innerHTML = article.title;
-			//Article
-			document.getElementById("artikel_text").innerHTML = article.first_paragraph;
-			//Thumbnailmage
-			document.getElementById("artikel_bild").innerHTML = "<img src='" + article.image_source + "'>";
-			//Categories
-			//document.getElementById("artikel_kategori").innerHTML = article.categories;
-
-			//console.log(article.categories);
-		
-
-			/*-----------------------------------------------
-					Tar ut information om artikeln 
-			-----------------------------------------------*/
-			document.getElementById("artikelinfo").innerHTML = "<b>Artikeltitel:</b> " + article.title
-			+ "<br><b>Artikel-Id: </b>" + article.id +"<br><br><b>Första paragrafen i artikeln: </b><br>" + article.first_paragraph + "<br><br>";
-			
-			//Kolla om det finns en position förknippad med artikeln eller inte.
-			if(article.position[0]) {
-
-				//document.getElementById("koordinater").innerHTML +=  "<b>Artikelns koordinater: </b>" + article.position;
-				addArticleToMap(article.position, article.title);
-
-			}
-
-
-			if(article.time[0]) {
-				document.getElementById("tidsinfo").innerHTML += "<b>Artikelns start och sluttid </b>" + article.time + "<br><br>";
-			}
-
-			
-			document.getElementById("länkar").innerHTML +=  "<b>Länkar i artikeln:</b> ("
-			+ article.links.length + " st)<br>";
-			for(var indx = 0; indx < article.links.length; indx++) {
-				document.getElementById("länkar").innerHTML += article.links[indx];
-				document.getElementById("länkar").innerHTML += ", ";
-			}
-
-			document.getElementById("länkar").innerHTML +=  "<br><br><b>Artiklar som länkar till denna artikel:</b> ("
-			+ article.backlinks.length + " st)<br>";
-			for(var indx = 0; indx < article.backlinks.length; indx++) {
-				document.getElementById("länkar").innerHTML += article.backlinks[indx];
-				document.getElementById("länkar").innerHTML += ", ";
-			}
-
-			document.getElementById("sökgenomförd").innerHTML = "Klicka på de olika tabbarna för mer information om artikeln.";
-		}
+	return birthplace;
+}
 
 
 
-		//Print the first sentence in an article.  
-		function getFirstRow(paragraph){
-			
-			//Find the position where a dot followed by space is in a string. 
-    		var n = paragraph.indexOf(". ");
-    		//Split the string where the position is set. 
-    		var res = paragraph.slice(0, n);
+function printArticle(article) {
 
-			//If you want to use the sentence in a javascript-file it's called this:
-    		var first_sentence = res; 
+	/*-----------------------------------------------
+			For the modal popup 
+	-----------------------------------------------*/
+	
+	document.getElementById("artikel_titel").innerHTML = article.title;								//Title
+	document.getElementById("artikel_text").innerHTML = article.first_paragraph;					//Article
+	document.getElementById("artikel_bild").innerHTML = "<img src='" + article.image_source + "'>";	//Thumbnailmage
+	
+	//Categories
+	//document.getElementById("artikel_kategori").innerHTML = article.categories;
 
-			return first_sentence;
-		}
+	//console.log(article.categories);
+
+
+	/*-----------------------------------------------
+			Gets information about the article 
+	-----------------------------------------------*/
+	document.getElementById("artikelinfo").innerHTML = "<b>Artikeltitel:</b> " + article.title
+	+ "<br><b>Artikel-Id: </b>" + article.id +"<br><br><b>Första paragrafen i artikeln: </b><br>" + article.first_paragraph + "<br><br>";
+	
+	//Check if the article has a position. 
+	if(article.position[0]) {
+
+		//document.getElementById("koordinater").innerHTML +=  "<b>Artikelns koordinater: </b>" + article.position;
+		addArticleToMap(article.position, article.title);
+
+	}
+
+
+	if(article.time[0]) {
+		document.getElementById("tidsinfo").innerHTML += "<b>Artikelns start och sluttid </b>" + article.time + "<br><br>";
+	}
+
+	
+	document.getElementById("links").innerHTML +=  "<b>länkar i artikeln:</b> ("
+	+ article.links.length + " st)<br>";
+	for(var indx = 0; indx < article.links.length; indx++) {
+		document.getElementById("links").innerHTML += article.links[indx];
+		document.getElementById("links").innerHTML += ", ";
+	}
+
+	document.getElementById("links").innerHTML +=  "<br><br><b>Artiklar som länkar till denna artikel:</b> ("
+	+ article.backlinks.length + " st)<br>";
+	for(var indx = 0; indx < article.backlinks.length; indx++) {
+		document.getElementById("links").innerHTML += article.backlinks[indx];
+		document.getElementById("links").innerHTML += ", ";
+	}
+
+	document.getElementById("searchcompleted").innerHTML = "Klicka på de olika tabbarna för mer information om artikeln.";
+}
+
+
+
+//Print the first sentence in an article.  
+function getFirstRow(paragraph){
+	
+	//Find the position where a dot followed by space is in a string. 
+	var n = paragraph.indexOf(". ");
+	//Split the string where the position is set. 
+	var res = paragraph.slice(0, n);
+
+	//If you want to use the sentence in a javascript-file it's called this:
+	var first_sentence = res; 
+
+	return first_sentence;
+}
 
 
 
--- js/links.js
+++ js/links.js
@@ -1,135 +1,137 @@
-/*******************************************************************************************************
- 	Allt det här låg i article.js förut, Hanna och Johanna har använt Albins kod och modifierat den.
-	Datum när filen skapades: 2016-04-05
+/*******************************************************************************************************	
+ 	Authors: Hanna Johansson and Johanna Westberg, mostly.
+ 	Code written by Albin Bergström has been used and modified.
+	Date of the file: 2016-04-05
 
-	Filen innehåller funktioner som hanterar länkarna i en artikel (links). 
-	Den ska helst hantera 'backlinks' sen också.
+	The file contains functions which handle the links and backlinks for an article.
 
+	This file includes the functions:
+	- getLinkSearch
+	- handleLinks
+	- linksPosition
+	- loadLinks
+	- printLinks
 ********************************************************************************************************/
 
+/*	The function check if the searchstring is empty and replace spaces with "%20",
+	because this is how Wikipedia's API works. The function then adds the searchstring in a query.
+	At last the final query is sent to the function 'searchWiki'.
 	
-		//Funktionen kollar att söksträngen inte är tom, byter ut mellanslag
-		//mot "%20" (för att wikipedia vill det) och lägger sedan in den i en hårdkodad query.
-		//Till sist skickas den färdiga query:n till funktionen "searchWiki".
+	This function works similar to the function 'getSearchString'. Used for the related links in the 
+	first search of an article. The functions defines which properties to get from the article.	*/
+function getLinkSearch(input_title) {
+	if(input_title) {
+		input_title = input_title.replace(" ", "%20");
 
-		//Typ en kopia av getSearchString. Används för de relaterade länkarna i den första sökningen.
-		//Här väljer man vilka 'properties' som hämtas från en viss artikel.
-		function getLinkSearch(input_title) {
-			if(input_title) {
-				input_title = input_title.replace(" ", "%20");
+		//The beginning of the query, tells us to do a query and return the result on json format.
+		var start = "/w/api.php?action=query&format=json";
+		//The what to search for
+		var title = "&titles=" + input_title;
+		//Which properties to get. (coordinates, links, revisions, extracts, pageid)
+		var properties = "&prop=coordinates" + "&indexpageids=1"; 			
+		//Create final query
+		var linkQuery = "http://sv.wikipedia.org" + start + title + properties + "&callback=?";
+	    return linkQuery;
+	}
+}
 
-				//The beginning of the query, tells us to do a query and return the result on json format.
-				var start = "/w/api.php?action=query&format=json";
-				//The what to search for
-				var title = "&titles=" + input_title;
-				//Which properties to get. (coordinates, links, revisions, extracts, pageid)
-				var properties = "&prop=coordinates" + "&indexpageids=1"; 			
-				//Create final query
-				var linkQuery = "http://sv.wikipedia.org" + start + title + properties + "&callback=?";
-			    return linkQuery;
-			}
+//Creates a new search for the links. 
+//The function handles at most 50 links/articles at the time.	
+function handleLinks(links){
+	first_time = false;
+
+	for(var indx = 0; indx < links.length; indx++){
+		var query = getLinkSearch(links[indx]);
+		searchWiki(query, first_time);
+	}
+} 
+
+//Checks if every link has a position, e.g. if it is a place.
+//If the link is a place, save the title under the id "places"
+function linksPosition(links){
+	for(var indx = 0; indx < links.length; indx++){
+		if(article.position != ""){
+			document.getElementById("places").innerHTML += article.title;
 		}
-
-		//Skapar nya sökningar av länkarna.
-		//Skicka en sökning av 50 länkar åt gången
-		function handleLinks(links){
-			first_time = false;
-
-			for(var indx = 0; indx < links.length; indx++){
-				var query = getLinkSearch(links[indx]);
-				searchWiki(query, first_time);
-			}
-
-			//ska sättas till true igen, ifall användaren klickar på en av länkarna?
-			//first_time = true;
-		} 
-
-		//Checkar om varje länk har en position, dvs. om den är en plats
-		//Om länken/artikeln är en plats, spara titeln under id "platser".
-		function linksPosition(links){
-
-			for(var indx = 0; indx < links.length; indx++){
-				if(article.position != ""){
-					document.getElementById("platser").innerHTML += article.title;
-				}
-			}
-		}
+	}
+}
 
 
-		//Typ en kopia av 'load'.
-		//Hämtar data för varje artikel/länk. Om artikeln är en plats/har koordinater sparas den i 'coord_articles'.
-		function loadLinks(data) {
+//Works similar as the function 'load'.
+//Gets data for every article/link. 
+//If the article is a place/has coordinates it is saved in the variable'coord_articles'.
+function loadLinks(data) {
 
-			var temp_article = {
-				title: "",
-				id: -1,
-				//first_paragraph: "",
-				position: [null,null],
-				birthplace: "",
-			}
+	var temp_article = {
+		title: "",
+		id: -1,
+		//first_paragraph: "",
+		position: [null,null],
+		birthplace: "",
+	}
 
-			temp_article.id = data.query.pageids[0];
-			temp_article.title = data.query.pages[temp_article.id].title;
-			//temp_article.first_paragraph = data.query.pages[temp_article.id].extract;
+	temp_article.id = data.query.pageids[0];
+	temp_article.title = data.query.pages[temp_article.id].title;
+	//temp_article.first_paragraph = data.query.pages[temp_article.id].extract;
 
-			all_articles.push(temp_article);
+	//Save all articles in the variable 'all_articles', e.g. all articles that are 'links' or 'backlinks'.
+	all_articles.push(temp_article);
 
-			//If the article has coordinates, save coordinates in 'position'
-			if(data.query.pages[temp_article.id].coordinates) {
-				temp_article.position =
-					[data.query.pages[temp_article.id].coordinates[0].lat,
-					 data.query.pages[temp_article.id].coordinates[0].lon]
-				
-				//Boolean to check if an article already exists in the array 'coord_articles' or not
-				var article_exist = false;
- 				
- 				//Lopp through the array to see if the article is already in the array, if so --> break and set boolean to true
- 				for( var i=0; i < coord_articles.length; i++){
- 					if (coord_articles[i].title == temp_article.title){
- 						article_exist = true;
- 						break;
- 					}						
- 				}
-				
-				//If the article does not exist in the array, push it into the array
-				if(!article_exist){
-					coord_articles.push(temp_article);
-				}
-				
-				addArticleToMap(temp_article.position, temp_article.title);
-
-			} 
-			else {
-				//If the article doesn't have coordinates, set them to null
-				temp_article.position = [null,null];
-			}
-
-			//Return array of articles which have coordinates, no duplicates in the array.
-			return coord_articles;
-		}
-
-
-		//Typ en kopia av printArticle
-		//Skriver ut information om länkarna.
-		function printLinks(linksarray) {
+	//If the article has coordinates, save coordinates in 'position'.
+	if(data.query.pages[temp_article.id].coordinates) {
+		temp_article.position =
+			[data.query.pages[temp_article.id].coordinates[0].lat,
+			 data.query.pages[temp_article.id].coordinates[0].lon]
+		
+		//Boolean to check if an article already exists in the array 'coord_articles' or not
+		var article_exist = false;
 			
-			var titles = [];
-			var linksCoord = [];
-			
-			//Loopa igenom arrayen för att skriva ut titlarna på alla länkar som har koordinater/platsangivelser
-			for(var indx = 0; indx < linksarray.length; indx++){
-				//document.getElementById("länkar").innerHTML = "<b>Artikeltitel:</b> " + linksarray[indx].title + "<br><br>";
-				titles.push(" " + linksarray[indx].title );
-				linksCoord.push(" " + linksarray[indx].position);
+			//Loop through the array to see if the article is already in the array, if so --> break and set boolean to true
+			for( var i=0; i < coord_articles.length; i++){
+				if (coord_articles[i].title == temp_article.title){
+					article_exist = true;
+					break;
+				}						
 			}
-
-			document.getElementById("länkar").innerHTML = titles;
-
-			//Om man vill skriva ut de relaterade länkarnas koordinater använd nedanstående rad.
-			//document.getElementById("länkar").innerHTML += "<br><br>" + linksCoord;
-
-			document.getElementById("länkar").innerHTML += "<br><br>" + "Antal länkar med koordinater:</b> " + linksarray.length;
+		
+		//If the article does not exist in the array, push it into the array
+		if(!article_exist){
+			coord_articles.push(temp_article);
 		}
+		
+		addArticleToMap(temp_article.position, temp_article.title);
+
+	} 
+	else {
+		//If the article doesn't have coordinates, set them to null
+		temp_article.position = [null,null];
+	}
+
+	//Return array of articles which have coordinates, no duplicates in the array.
+	return coord_articles;
+}
+
+
+//Works similar to 'printArticle'. Prints out information about the links/articles.
+function printLinks(linksarray) {
+	
+	var titles = [];
+	var linksCoord = [];
+	
+	//Loop through the array to print all links that have coordinates
+	for(var indx = 0; indx < linksarray.length; indx++){
+		//document.getElementById("links").innerHTML = "<b>Artikeltitel:</b> " + linksarray[indx].title + "<br><br>";
+		titles.push(" " + linksarray[indx].title );
+		linksCoord.push(" " + linksarray[indx].position);
+	}
+
+	document.getElementById("links").innerHTML = titles;
+
+	//If the coordinates to the related articles should be printed, use this line.
+	//document.getElementById("links").innerHTML += "<br><br>" + linksCoord;
+
+	document.getElementById("links").innerHTML += "<br><br>" + "Antal links med koordinater:</b> " + linksarray.length;
+}
 
 
 	
